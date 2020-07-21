ea351b35c1a9bd71eef2cfd6a2501954060a8175
novak <novakivanovski@gmail.com>
2020-07-21 00:25:46 -0400

    test

--- src/main/java/io/github/novakivanovski/autoreplant/AutoReplantPlugin.java
+++ src/main/java/io/github/novakivanovski/autoreplant/AutoReplantPlugin.java
@@ -1,11 +1,15 @@
 package io.github.novakivanovski.autoreplant;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AutoReplantPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.getCommand("autoreplant").setExecutor(new CommandEnable());
+        PluginCommand command = this.getCommand("autoreplant");
+        if (command != null) {
+            command.setExecutor(new CommandEnable());
+        }
     }
 
     @Override
