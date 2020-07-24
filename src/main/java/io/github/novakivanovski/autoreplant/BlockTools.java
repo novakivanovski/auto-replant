86a2e067ce652aedee1ae1e35671b48351d7f7d4
novak <novakivanovski@gmail.com>
2020-07-24 00:37:46 -0400

    refactor

--- src/main/java/io/github/novakivanovski/autoreplant/BlockTools.java
+++ src/main/java/io/github/novakivanovski/autoreplant/BlockTools.java
@@ -4,6 +4,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Chest;
+import org.bukkit.block.data.Ageable;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Switch;
 
 import java.util.ArrayList;
@@ -48,20 +52,26 @@ public class BlockTools {
         return block.getRelative(behind);
     }
 
-    public static Block getAdjacentChest(Block block) {
-        return getAdjacentBlockOfType(block, Material.CHEST);
+    public static Chest getAdjacentChest(Block block) {
+        Block chestBlock = getAdjacentBlockOfType(block, Material.CHEST);
+        if (chestBlock == null) return null;
+
+        return (Chest) chestBlock.getState();
     }
 
-    public static void harvestCrops(List<Block> farmlandBlocks) {
+    public static int harvestCrops(List<Block> farmlandBlocks) {
         int harvest = 0;
         for (Block block : farmlandBlocks) {
             Block aboveBlock = block.getRelative(BlockFace.UP);
             if (aboveBlock.getType() == Material.WHEAT) {
-                aboveBlock.setType(Material.AIR);
-                harvest += 1;
+                Ageable crop = (Ageable) aboveBlock.getBlockData();
+                if (crop.getAge() == crop.getMaximumAge()) {
+                    harvest += 1;
+                    crop.setAge(0);
+                }
             }
         }
-        Bukkit.broadcastMessage("Harvest count: " + harvest);
+        return harvest;
     }
 
 }
--- src/main/java/io/github/novakivanovski/autoreplant/CropBreakListener.java
+++ src/main/java/io/github/novakivanovski/autoreplant/CropBreakListener.java
@@ -4,10 +4,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.Chest;
 import org.bukkit.block.data.type.Switch;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
 import java.util.List;
 
 
@@ -19,10 +24,14 @@ public class CropBreakListener implements Listener {
         if (block.getType() == Material.LEVER) {
             Block redstoneBlock = BlockTools.getBlockBehindLever(block);
             if (redstoneBlock.getType() == Material.REDSTONE_BLOCK) {
+                Chest chest = BlockTools.getAdjacentChest(redstoneBlock);
+                if (chest == null) return;
                 BlockFace direction = BlockTools.getDirectionOfBlock(redstoneBlock, Material.FARMLAND);
                 if (direction == null) return;
                 List<Block> farm = BlockTools.getConsecutiveBlocksOfType(redstoneBlock, direction, Material.FARMLAND);
-                BlockTools.harvestCrops(farm);
+                int harvestCount = BlockTools.harvestCrops(farm);
+                ItemStack crops = new ItemStack(Material.WHEAT, harvestCount);
+                chest.getInventory().addItem(crops);
             }
         }
     }
