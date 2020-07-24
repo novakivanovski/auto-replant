b2df21aa675f0cc206ca117e23bc6e7febc6a0e9
novak <novakivanovski@gmail.com>
2020-07-24 00:39:58 -0400

    clean up

--- src/main/java/io/github/novakivanovski/autoreplant/AutoReplantPlugin.java
+++ src/main/java/io/github/novakivanovski/autoreplant/AutoReplantPlugin.java
@@ -1,15 +1,10 @@
 package io.github.novakivanovski.autoreplant;
-import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AutoReplantPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        PluginCommand command = this.getCommand("autoreplant");
-        if (command != null) {
-            command.setExecutor(new CommandEnable());
-        }
         getServer().getPluginManager().registerEvents(new CropBreakListener(), this);
     }
 
--- src/main/java/io/github/novakivanovski/autoreplant/CropBreakListener.java
+++ src/main/java/io/github/novakivanovski/autoreplant/CropBreakListener.java
@@ -1,16 +1,11 @@
 package io.github.novakivanovski.autoreplant;
-import org.bukkit.BanList;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Chest;
-import org.bukkit.block.data.type.Switch;
-import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockRedstoneEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
