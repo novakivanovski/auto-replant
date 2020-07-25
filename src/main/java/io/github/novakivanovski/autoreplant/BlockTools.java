67b042680e903203a60d291c5b273ba1187f545b
novak <novakivanovski@gmail.com>
2020-07-25 00:35:03 -0400

    use optional

--- src/main/java/io/github/novakivanovski/autoreplant/BlockTools.java
+++ src/main/java/io/github/novakivanovski/autoreplant/BlockTools.java
@@ -1,17 +1,16 @@
 package io.github.novakivanovski.autoreplant;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
 import org.bukkit.block.data.Ageable;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Switch;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class BlockTools {
 
@@ -20,19 +19,19 @@ public class BlockTools {
             BlockFace.WEST,
             BlockFace.SOUTH};
 
-    public static Block getAdjacentBlockOfType(Block block, Material material) {
+    public static Optional<Block> getAdjacentBlockOfType(Block block, Material material) {
         for (BlockFace direction: directions) {
             Block adjacentBlock = block.getRelative(direction);
-            if (adjacentBlock.getType() == material) return adjacentBlock;
+            if (adjacentBlock.getType() == material) return Optional.of(adjacentBlock);
         }
-        return null;
+        return Optional.empty();
     }
 
-    public static BlockFace getDirectionOfBlock(Block block, Material material) {
+    public static Optional<BlockFace> getDirectionOfBlock(Block block, Material material) {
         for (BlockFace direction : directions) {
-            if (block.getRelative(direction).getType() == material) return direction;
+            if (block.getRelative(direction).getType() == material) return Optional.of(direction);
         }
-        return null;
+        return Optional.empty();
     }
 
 
@@ -52,11 +51,9 @@ public class BlockTools {
         return block.getRelative(behind);
     }
 
-    public static Chest getAdjacentChest(Block block) {
-        Block chestBlock = getAdjacentBlockOfType(block, Material.CHEST);
-        if (chestBlock == null) return null;
-
-        return (Chest) chestBlock.getState();
+    public static Optional<Chest> getAdjacentChest(Block block) {
+        Optional<Block> chestBlock = getAdjacentBlockOfType(block, Material.CHEST);
+        return chestBlock.map(value -> (Chest) value.getState());
     }
 
     public static int harvestCrops(List<Block> farmlandBlocks) {
--- src/main/java/io/github/novakivanovski/autoreplant/CropBreakListener.java
+++ src/main/java/io/github/novakivanovski/autoreplant/CropBreakListener.java
@@ -7,8 +7,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockRedstoneEvent;
 import org.bukkit.inventory.ItemStack;
-
 import java.util.List;
+import java.util.Optional;
 
 
 public class CropBreakListener implements Listener {
@@ -19,14 +19,14 @@ public class CropBreakListener implements Listener {
         if (block.getType() == Material.LEVER) {
             Block redstoneBlock = BlockTools.getBlockBehindLever(block);
             if (redstoneBlock.getType() == Material.REDSTONE_BLOCK) {
-                Chest chest = BlockTools.getAdjacentChest(redstoneBlock);
-                if (chest == null) return;
-                BlockFace direction = BlockTools.getDirectionOfBlock(redstoneBlock, Material.FARMLAND);
-                if (direction == null) return;
-                List<Block> farm = BlockTools.getConsecutiveBlocksOfType(redstoneBlock, direction, Material.FARMLAND);
+                Optional<Chest> chest = BlockTools.getAdjacentChest(redstoneBlock);
+                Optional<BlockFace> direction = BlockTools.getDirectionOfBlock(redstoneBlock, Material.FARMLAND);
+                if (chest.isEmpty() || direction.isEmpty()) return;
+                List<Block> farm = BlockTools.getConsecutiveBlocksOfType(redstoneBlock, direction.get(), Material.FARMLAND);
                 int harvestCount = BlockTools.harvestCrops(farm);
                 ItemStack crops = new ItemStack(Material.WHEAT, harvestCount);
-                chest.getInventory().addItem(crops);
+                chest.map(Chest::getInventory)
+                        .ifPresent(inventory -> inventory.addItem(crops));
             }
         }
     }
